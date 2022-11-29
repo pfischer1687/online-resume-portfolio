edc683cc6223e710bb99cb794850d5efc67d5c26
pfischer1687 <pfisch789@outlook.com>
2022-11-28 23:10:24 -0800

    Adds contact links to nav menu in Layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,22 @@
 import * as React from "react";
 import * as styles from "./layout.module.css";
 import Header from "./header";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Layout = ({ children }) => {
   React.useEffect(() => {
     const navTerminal = document.getElementById("nav-terminal");
-    setTimeout(() => navTerminal.classList.add(`${styles.show}`), 1000);
+    const navTerminalLs = document.getElementById("nav-terminal-ls");
+    const navLinks = document.getElementById("nav-links");
+    const currTerminal = document.getElementById("curr-terminal");
+    setTimeout(() => {
+      navTerminal.classList.add(`${styles.show}`);
+      navTerminalLs.classList.add(`${styles.typing}`);
+    }, 1000);
+    setTimeout(() => {
+      navLinks.classList.add(`${styles.show}`);
+      currTerminal.classList.add(`${styles.show}`);
+    }, 4000);
   });
 
   return (
@@ -15,12 +26,78 @@ const Layout = ({ children }) => {
         <h3 id="nav-terminal" className={styles.hidden}>
           <span className={styles.terminalUsr}>guest@guest</span>:
           <span className={styles.terminalPath}>~</span>${" "}
-          <span className={styles.typing}>ls</span>
+          <span id="nav-terminal-ls">ls</span>
         </h3>
-        <h3>
-          <a href="#about">about</a> <a href="#contact">contact</a>{" "}
-          <a href="#education">education</a>{" "}
-          <a href="#experience">experience</a> <a href="#projects">projects</a>
+        <h3 id="nav-links" className={styles.hidden}>
+          <a href="#about">about</a> <a href="#education">education</a>{" "}
+          <a href="#experience">experience</a> <a href="#projects">projects</a>{" "}
+          <div className={styles.contactLinks}>
+            <a
+              href="https://github.com/pfischer1687"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <StaticImage
+                  height={50}
+                  src="../images/github-logo.png"
+                  alt="GitHub logo"
+                />
+              </div>
+              <div>GitHub</div>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/paul-fischer-816042127"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <StaticImage
+                  height={50}
+                  src="../images/linkedin-logo.png"
+                  alt="LinkedIn logo"
+                />
+              </div>
+              <div>LinkedIn</div>
+            </a>
+            <a
+              href="https://twitter.com/PaulFis43236408"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <StaticImage
+                  height={50}
+                  src="../images/twitter-logo.png"
+                  alt="Twitter logo"
+                />
+              </div>
+              <div>Twitter</div>
+            </a>
+            <a
+              href="mailto:paulfischerdev@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <StaticImage
+                  height={50}
+                  src="../images/gmail-logo.png"
+                  alt="Gmail logo"
+                  style={{ filter: "invert(100%)" }}
+                />
+              </div>
+              <div>paulfischerdev@gmail.com</div>
+            </a>
+            {/* <a href="mailto:paulfischerdev@gmail.com">
+              paulfischerdev@gmail.com
+            </a> */}
+          </div>
+        </h3>
+        <h3 id="curr-terminal" className={styles.hidden}>
+          <span className={styles.terminalUsr}>guest@guest</span>:
+          <span className={styles.terminalPath}>~</span>${" "}
+          <span className={styles.typingInf}>&nbsp;</span>
         </h3>
       </nav>
       <main>{children}</main>
