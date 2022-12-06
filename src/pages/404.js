e5f92f224aa8ad5fa913466889ba16c333fa9384
pfischer1687 <pfisch789@outlook.com>
2022-12-05 17:27:34 -0800

    Adds content to the 404 page and gets rid of index page content

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,167 +1,67 @@
-// import * as React from "react";
-// import Layout from "../components/layout";
-// import { StaticImage } from "gatsby-plugin-image";
-// import Seo from "../components/seo";
-// import * as styles from "../components/about.module.css";
-
-// const NotFoundPage = () => {
-//   return (
-//     <Layout>
-//       <div className={styles.contentWrap}>
-//         <h2>404: Page Not Found</h2>
-//         <p>
-//           Sorry, we couldn’t find what you were looking for. Please enjoy this
-//           picture of a cute dog and then click any of the navigation links above
-//           or footer links below to navigate through the site.
-//         </p>
-//         <StaticImage
-//           alt="Cute gray dog lying in a driveway with its tongue sticking out"
-//           src="../images/dog-pic.jpg"
-//         />
-//         <p>
-//           Photo by{" "}
-//           <a
-//             href="https://unsplash.com/@mutedtone?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-//             target="_blank"
-//             rel="noreferrer"
-//           >
-//             Christopher Ayme
-//           </a>{" "}
-//           on{" "}
-//           <a
-//             href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-//             target="_blank"
-//             rel="noreferrer"
-//           >
-//             Unsplash
-//           </a>
-//         </p>
-//       </div>
-//     </Layout>
-//   );
-// };
-
-// export const Head = () => <Seo title="404: Page not found" />;
-
-// export default NotFoundPage;
-
 import * as React from "react";
-import * as styles from "../components/about.module.css";
-import { StaticImage } from "gatsby-plugin-image";
+import * as styles from "../components/experience.module.css";
 import Seo from "../components/seo";
 import Layout from "../components/layout";
+import TypingAnimation from "../components/typing-animation";
+import { StaticImage } from "gatsby-plugin-image";
 
-const catTerminalTime = 3000;
-const contentTime = 6000;
-const footerTime = 7000;
-
-const AboutPage = () => {
+const NotFoundPage = () => {
+  const pageTitle = "404";
   React.useEffect(() => {
-    const catAboutTerminal = document.getElementById("cat-about-terminal");
-    const catAboutText = document.getElementById("cat-about-text");
-    const aboutContent = document.getElementById("about-content");
-    setTimeout(() => {
-      catAboutTerminal.classList.add(`${styles.show}`);
-      catAboutText.classList.add(`${styles.typingCatAbout}`);
-    }, catTerminalTime);
-    setTimeout(() => {
-      aboutContent.classList.add(`${styles.show}`);
-    }, contentTime);
+    TypingAnimation();
   });
 
   return (
-    <Layout footerTerminalTime={footerTime}>
-      <div className={`${styles.contentWrap} ${styles.terminalCode}`}>
+    <Layout pagePath={`/${pageTitle}`}>
+      <div className="content-wrap terminal-code">
         <h3>
-          <span className={styles.terminalUsr}>guest@guest</span>:
-          <span className={styles.terminalPath}>~</span>${" "}
-          <span className={styles.typingCdAbout}>cd ~/about</span>
+          <span className="terminal-usr">guest@guest</span>:
+          <span className="terminal-path">~</span>${" "}
+          <span className={`typing ${styles.typingCd}`}>cd ~/{pageTitle}</span>
         </h3>
-        <h3 id="cat-about-terminal" className={styles.hidden}>
-          <span className={styles.terminalUsr}>guest@guest</span>:
-          <span className={styles.terminalPath}>~/about</span>${" "}
-          <span id="cat-about-text">cat about.txt</span>
+        <h3 id="cat-terminal" className="hidden">
+          <span className="terminal-usr">guest@guest</span>:
+          <span className="terminal-path">~/{pageTitle}</span>${" "}
+          <span id="cat-text" className={styles.typingCat}>
+            cat {pageTitle}.txt
+          </span>
         </h3>
       </div>
 
-      <section
-        id="about-content"
-        className={`${styles.textFile} ${styles.contentWrap} ${styles.hidden}`}
-      >
-        <h2 className={styles.aboutMe}>About Me</h2>
-        <section className={styles.aboutItem}>
-          <div>
-            <StaticImage
-              className={styles.aboutImg}
-              src="../images/about-me.jpg"
-              alt="Photo of me."
-            />
-          </div>
-
-          <div className={styles.aboutDetails}>
-            <p>
-              Hi! I'm Paul. I love to code everything from websites to
-              computational physics simulations, financial data science,
-              embedded systems in arduino, and more!
-            </p>
-            <p>
-              After graduating with a B.S. in Physics with a minor in Applied
-              Mathematics, I taught high school Physics and AP Physics. I
-              enjoyed sharing my knowledge but wanted to return to do more
-              research. During my M.S. in Computational Physics, I really
-              developed my love of coding. My research involved working with
-              Markov chain Monte Carlo simulations in the C programming language
-              distributed among several nodes of a High Performance Cluster
-              (HPC) in parallel via Open MPI on Red Hat Enterprise Linux. The
-              analysis of my data was done in Python and utilized the libraries
-              pandas, NumPy, SciPy, SymPy, and Matplotlib. My love f coding for
-              my research drew me to my current career goals in software
-              engineering.
-            </p>
-            <h3>Top Skills:</h3>
-            <div className={styles.skillIcons}>
-              <div>
-                <StaticImage
-                  style={{ borderRadius: "5px" }}
-                  src="../images/js-logo.png"
-                  alt="JavaScript logo"
-                />
-                <div>JavaScript</div>
-              </div>
-              <div>
-                <StaticImage
-                  src="../images/react-logo.png"
-                  alt="React.js logo"
-                />
-                <div>React.js</div>
-              </div>
-              <div>
-                <StaticImage
-                  src="../images/python-logo.png"
-                  alt="Python logo"
-                />
-                <div>Python</div>
-              </div>
-              <div>
-                <StaticImage
-                  src="../images/c-logo.png"
-                  alt="The C programming language logo"
-                />
-                <div>C</div>
-              </div>
-
-              <div>
-                <StaticImage src="../images/linux-logo.png" alt="Linux logo" />
-                <div>Linux</div>
-              </div>
-            </div>
-          </div>
-        </section>
+      <section id="content" className="content-wrap hidden">
+        <h2>404: Page Not Found</h2>
+        <p>
+          Sorry, we couldn’t find what you were looking for. Please enjoy this
+          picture of a cute dog and then click any of the navigation links above
+          or footer links below to navigate through the site.
+        </p>
+        <StaticImage
+          alt="Cute gray dog lying in a driveway with its tongue sticking out"
+          src="../images/dog-pic.jpg"
+        />
+        <p>
+          Photo by{" "}
+          <a
+            href="https://unsplash.com/@mutedtone?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Christopher Ayme
+          </a>{" "}
+          on{" "}
+          <a
+            href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Unsplash
+          </a>
+        </p>
       </section>
     </Layout>
   );
 };
 
-export const Head = () => <Seo title="About" />;
+export const Head = () => <Seo title="404: Page not found" />;
 
-export default AboutPage;
+export default NotFoundPage;
---
+++ src/pages/experience.js
@@ -0,0 +1,97 @@
+import * as React from "react";
+import * as styles from "../components/experience.module.css";
+import Seo from "../components/seo";
+import Layout from "../components/layout";
+import TypingAnimation from "../components/typing-animation";
+
+const ExperiencePage = () => {
+  const pageTitle = "experience";
+  React.useEffect(() => {
+    TypingAnimation();
+  });
+
+  return (
+    <Layout pagePath={`/${pageTitle}`}>
+      <div className="content-wrap terminal-code">
+        <h3>
+          <span className="terminal-usr">guest@guest</span>:
+          <span className="terminal-path">~</span>${" "}
+          <span className={`typing ${styles.typingCd}`}>cd ~/{pageTitle}</span>
+        </h3>
+        <h3 id="cat-terminal" className="hidden">
+          <span className="terminal-usr">guest@guest</span>:
+          <span className="terminal-path">~/{pageTitle}</span>${" "}
+          <span className="typing-cat-line-break"></span>
+          <span id="cat-text" className={styles.typingCat}>
+            cat {pageTitle}.txt
+          </span>
+        </h3>
+      </div>
+
+      <section
+        id="content"
+        className={`text-file content-wrap ${styles.experience} divider hidden`}
+      >
+        <h2>Work Experience</h2>
+
+        {/* <!-- Job 1 --> */}
+        <section>
+          <div className={styles.jobDetails}>
+            <h3>
+              Graduate Assistant(PHYS 510/562)/Teaching Associate (PHYS
+              100A/151)
+            </h3>
+            <p>California State University, Long Beach</p>
+            <p>August 2019 -Present</p>
+          </div>
+
+          <div>
+            <p>
+              Taught statistical methods for analyzing and inferring conclusions
+              fromexperimental physics data gathered in Microsoft Excel
+              andcomputational experiments simulated in VPython. Measured
+              student performance viagrades and providedtimely,
+              constructivefeedback. Encouragedequalcollaboration and effective
+              communication among students working in groups.
+            </p>
+          </div>
+          {/* <!--
+          <p>Optional list:</p>
+          <ul>
+            <li>Delete this list if you don't need it.</li>
+            <li>Created...</li>
+            <li>Lead...</li>
+            <li>Responsible for...</li>
+          </ul>
+          --> */}
+        </section>
+
+        {/* <!-- Job 2 --> */}
+        <section>
+          <div className={styles.jobDetails}>
+            <h3>Physics/AP Physics Teacher</h3>
+            <p>Saint Monica Catholic High School</p>
+            <p>August2016 –June 2019</p>
+          </div>
+          <div>
+            <p>
+              Developed and managedstudent laboratory projects analyzingrecorded
+              datain Excelagainst predictive models, occasionallyinvolving real
+              data sets from NASA. Attended monthly professional development
+              events (such as College Board Workshops, the CA STEAM Symposium,
+              and AAPT meetings) to better develop the skill ofeffectively
+              communicatingtechnical ideasto a non-technical audience. Helped
+              design the new makerspace, including strategies to encourage
+              participation from underrepresented groups in science.
+            </p>
+          </div>
+        </section>
+        {/* <!-- End of Job block. --> */}
+      </section>
+    </Layout>
+  );
+};
+
+export const Head = () => <Seo title="Experience" />;
+
+export default ExperiencePage;
