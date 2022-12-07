7c7532fb43c8be1e381ed3b35aa85724b3dae52e
pfischer1687 <pfisch789@outlook.com>
2022-12-06 21:46:59 -0800

    Reformats header to use content-wrap style from global CSS

--- src/components/header.js
+++ src/components/header.js
@@ -5,18 +5,21 @@ import { Link } from "gatsby";
 
 const Header = () => (
   <header>
-    <div className={styles.contentWrap}>
-      <Link to="/">
-        <StaticImage
-          src="../images/favicon.png"
-          alt="PF logo"
-          className={styles.logo}
-        />
-      </Link>
-      <div>
-        <h1>Paul Fischer</h1>
-        <h2>Software Engineer</h2>
-        <h2>Physicist</h2>
+    {/* <div className={styles.contentWrap}> */}
+    <div className="content-wrap">
+      <div className={styles.headerContainer}>
+        <Link to="/">
+          <StaticImage
+            src="../images/favicon.png"
+            alt="PF logo"
+            className={styles.logo}
+          />
+        </Link>
+        <div>
+          <h1>Paul Fischer</h1>
+          <h2>Software Engineer</h2>
+          <h2>Physicist</h2>
+        </div>
       </div>
     </div>
   </header>
