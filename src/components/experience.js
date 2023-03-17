dd6a2300f9fa1c93446a6c25f6cd0c41a2150568
pfischer1687 <pfisch789@outlook.com>
2023-03-16 23:36:02 -0700

    Updates content in Experience component

--- src/components/experience.js
+++ src/components/experience.js
@@ -11,58 +11,35 @@ const Experience = () => {
       precedesFooter={true}
     >
       <section className={`${styles.experience} divider`}>
-        <h2>Work Experience</h2>
+        <h2>Experience</h2>
 
         {/* <!-- Job 1 --> */}
         <section>
           <div className={styles.jobDetails}>
-            <h3>
-              Graduate Assistant(PHYS 510/562)/Teaching Associate (PHYS
-              100A/151)
-            </h3>
-            <p>California State University, Long Beach</p>
-            <p>August 2019 -Present</p>
+            <h3>Graduate Assistant/Teaching Associate</h3>
+            <h3>August 2019 – May 2022</h3>
+            <h3>CSULB Physics Department | Long Beach, CA</h3>
+            <ul>
+              <li>
+                Led students through the development and testing of
+                computational physics simulations in Python
+              </li>
+            </ul>
           </div>
-
-          <div>
-            <p>
-              Taught statistical methods for analyzing and inferring conclusions
-              fromexperimental physics data gathered in Microsoft Excel
-              andcomputational experiments simulated in VPython. Measured
-              student performance viagrades and providedtimely,
-              constructivefeedback. Encouragedequalcollaboration and effective
-              communication among students working in groups.
-            </p>
-          </div>
-          {/* <!--
-  <p>Optional list:</p>
-  <ul>
-    <li>Delete this list if you don't need it.</li>
-    <li>Created...</li>
-    <li>Lead...</li>
-    <li>Responsible for...</li>
-  </ul>
-  --> */}
         </section>
 
         {/* <!-- Job 2 --> */}
         <section>
           <div className={styles.jobDetails}>
             <h3>Physics/AP Physics Teacher</h3>
-            <p>Saint Monica Catholic High School</p>
-            <p>August2016 –June 2019</p>
-          </div>
-          <div>
-            <p>
-              Developed and managedstudent laboratory projects analyzingrecorded
-              datain Excelagainst predictive models, occasionallyinvolving real
-              data sets from NASA. Attended monthly professional development
-              events (such as College Board Workshops, the CA STEAM Symposium,
-              and AAPT meetings) to better develop the skill ofeffectively
-              communicatingtechnical ideasto a non-technical audience. Helped
-              design the new makerspace, including strategies to encourage
-              participation from underrepresented groups in science.
-            </p>
+            <h3>August 2016 – June 2019</h3>
+            <h3>Saint Monica Catholic High School | Santa Monica, CA</h3>
+            <ul>
+              <li>
+                Directed students on how to leverage statistical techniques for
+                data analysis in Microsoft Excel
+              </li>
+            </ul>
           </div>
         </section>
         {/* <!-- End of Job block. --> */}
